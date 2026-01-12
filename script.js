e0f52153f658b3ac821eab8315e3065aa1bbc8eb
Kai <kaibomholtz@gmail.com>
2026-01-12 19:43:17 +0100

    created the main logic

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-function knightsMoves([start], [end]) {
-
+class Node {
+    constructor(position, path = []) {
+        this.position = position;
+        this.path = path;
+    }
 }
 
-function buildBoard() {
-    let board = []
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            board.push([i,j])
+function knightsMoves(start, end) {
+    // Initialize the queue
+    let queue = [[start, [start]]];
+
+    // Track visited squares
+    // Convert to string for comparison
+    let visited = new Set();
+    visited.add(start.toString());
+
+    while (queue.length > 0) {
+        let [currentPos, path] = queue.shift();
+
+        // Check if we reached goal
+        if (currentPos[0] === end[0] && currentPos[1] === end[1]) {
+            console.log(`You made it in ${path.length - 1} moves! Here's your path:`)
+            path.forEach(pos => console.log(pos));
+            return path;
+        }
+
+        // Explore neighbors 
+        let moves = getLegalMoves(currentPos);
+        for (let move of moves) {
+            if (!visited.has(move.toString())) {
+                visited.add(move.toString());
+                queue.push([move, [...path, move]]);
+            }
         }
     }
-    return board
+
 }
 
-console.log(buildBoard())
+function getLegalMoves(pos) {
+    const [x, y] = pos;
+    // All 8 possible "L" shapes a knight can make
+    const potentialMoves = [
+        [x + 2, y + 1], [x + 2, y - 1],
+        [x - 2, y + 1], [x - 2, y - 1],
+        [x + 1, y + 2], [x + 1, y - 2],
+        [x - 1, y + 2], [x - 1, y - 2]
+    ];
+
+    // Filter to keep moves inside the 8x8 board
+    return potentialMoves.filter(move => 
+        move[0] >= 0 && move[0] <= 7 && 
+        move[1] >= 0 && move[1] <= 7
+    );
+}
+
+knightsMoves([0,0], [7,2])
+
+
